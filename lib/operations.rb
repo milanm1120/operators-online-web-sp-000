2e963eed5a424e57b2c6082cb7bd78036f67e1d1
Milan Mody <milanm1120@gmail.com>
2021-02-12 01:54:35 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,23 @@
+
+  #Operations
+    #unsafe?
+      #returns true if speed is greater than 60
+      #returns true if speed is less than 40
+      #returns false if the speed is between 40 and 60
+    #not_safe?
+      #returns true if speed is greater than 60
+      #returns true if the speed is less than 40
+      #returns false if the speed is between 40 and 60
+      #uses the ternary operator
+
 def unsafe?(speed)
-  if speed > 60 || speed < 40
-    true
-  else
+  if  speed>=40 && speed<=60
     false
+  else
+    true
   end
 end
 
-
-
 def not_safe?(speed)
-  speed > 60 || speed < 40 ? true : false 
+  speed < 40 || speed > 60? true : false
 end
